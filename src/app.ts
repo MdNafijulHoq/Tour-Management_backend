0e56f93a2e78b82ca41052c953295075453ce9c4
Nafijul Haque Nafis <nafijulhaque118@gmail.com>
2025-08-02 23:28:32 +0600

    part-3 completed

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,26 @@ import cors from "cors";
 import { router } from "./app/router";
 import { globalErrorHandler } from "./app/middlewares/globalErrorhandler";
 import notFound from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
+import "./app/config/passport";
+import { envVars } from "./app/config/env";
+
 const app = express();
+
+app.use(
+  expressSession({
+    secret: envVars.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/v1", router);
 
@@ -15,8 +32,8 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-app.use(notFound)
+app.use(notFound);
 
 export default app;
--- src/app/modules/auth/auth.controllers.ts
+++ src/app/modules/auth/auth.controllers.ts
@@ -2,23 +2,135 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import httpStatus from 'http-status-codes';
+import httpStatus from "http-status-codes";
 import { AuthServices } from "./auth.service";
+import AppError from "../../errorHeleprs/AppError";
+import { setAuthCookie } from "../../utils/setCookie";
+import { JwtPayload } from "jsonwebtoken";
+import { createUserTokens } from "../../utils/userToken";
+import { envVars } from "../../config/env";
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const loginInfo = await AuthServices.credentialsLogin(req.body)
+    const loginInfo = await AuthServices.credentialsLogin(req.body);
+
+    // res.cookie("accessToken", loginInfo.accessToken, {
+    //   httpOnly: true,
+    //   secure: false,
+    // });
+    // res.cookie("refreshToken", loginInfo.refreshToken, {
+    //   httpOnly: true,
+    //   secure: false,
+    // });
+
+    setAuthCookie(res, loginInfo);
+
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
-      message: "All Users Retrieved Successfully",
+      message: "Users Logged In Successfully",
       data: loginInfo,
     });
   }
 );
 
+const getNewAccessToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
 
+    if (!refreshToken) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "No refresh token received from cookies"
+      );
+    }
+
+    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken);
+
+    // res.cookie("accessToken", tokenInfo.accessToken, {
+    //   httpOnly: true,
+    //   secure: false,
+    // });
+    setAuthCookie(res, tokenInfo);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "New Access Token Retrived Successfully",
+      data: tokenInfo,
+    });
+  }
+);
+
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "All Logged Out Successfully",
+      data: null,
+    });
+  }
+);
+
+const resetPassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+
+    const oldPassword = req.body.oldPassword;
+    const newPassword = req.body.newPassword;
+    const decodedToken = req.user;
+    
+    await AuthServices.resetPassword(oldPassword, newPassword, decodedToken as JwtPayload)
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Password Changed Successfully",
+      data: null,
+    });
+  }
+);
+
+const googleCallbackController = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+
+   let redirectTo = req.query.state ? req.query.state as string : "" ;
+   if(redirectTo.startsWith("/")){
+     redirectTo = redirectTo.slice(1)
+   }
+
+   const user = req.user;
+   // eslint-disable-next-line no-console
+   console.log("user", user);
+
+   if(!user){
+    throw new AppError(httpStatus.NOT_FOUND, "User not found")
+   }
+
+   const tokenInfo = createUserTokens(user)
+
+   setAuthCookie(res, tokenInfo)
+   
+   res.redirect(`${envVars.FRONTEND_URL}/${redirectTo}`)
+  }
+);
 
 export const AuthControllers = {
-    credentialsLogin,
-}
+  credentialsLogin,
+  getNewAccessToken,
+  logout,
+  resetPassword,
+  googleCallbackController
+};
